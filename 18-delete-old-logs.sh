7bafd0ca7627f2df51d84a119f13102c19064776
lakshman <lakshmannadendla15122021@gmail.com>
2025-11-06 11:04:55 +0530

    scripting

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SOURSE_DIRECTORY=/tmp/app_logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURSE_DIRECTORY ]
+then 
+    echo -e "$G Sourse directory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi 
+
+FILES=$(find $COURSE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do 
+    echo "Deleting file: $line"
+    #rm -rf $line"
+done <<< $FILES
